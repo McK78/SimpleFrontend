e37e634f3686cf9a98eb7ac1581cddf9a25b338a
McK78 <chandumediga007@gmail.com>
2026-06-08 17:46:22 +0530

    fannin replica v1.0

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  const forms = document.querySelectorAll("form");
+
+  forms.forEach(form => {
+
+    form.addEventListener("submit", () => {
+
+      const button = form.querySelector("button");
+
+      button.innerText = "Submitting...";
+      button.disabled = true;
+
+      setTimeout(() => {
+        button.innerText = "Submitted";
+      }, 1000);
+
+    });
+
+  });
+
+});
